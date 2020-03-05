fca94c16db606d380c7c253dde50f69fc27e9471
Anas <anbellouzi@gmail.com>
2020-03-04 21:27:26 -0800

    development

--- src/index.js
+++ src/index.js
@@ -9,14 +9,18 @@ const router = require('./controllers/index.js');
 mongoose.Promise = Promise;
 
 // connect to mongo db
-const mongoUri = "mongodb://localhost:27017/auth-api-starterpack";
-mongoose.connect(
-  mongoUri,
-  { server: { socketOptions: { keepAlive: 1 } } }
-);
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${mongoUri}`);
-});
+// const mongoUri = "mongodb://localhost:27017/auth-api-starterpack";
+const mongo_uri = process.env.MONGODB_URI
+// mongoose.connect(
+//   mongoUri,
+//   { server: { socketOptions: { keepAlive: 1 } } }
+// );
+// mongoose.connection.on('error', () => {
+//   throw new Error(`unable to connect to database: ${mongoUri}`);
+// });
+
+
+mongoose.connect(mongo_uri)
 
 // # TODO: Any additional config changes belong here.
 
@@ -28,9 +32,12 @@ app.use(router);
 // src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
   // listen on port config.port
-  app.listen(4040, () => {
-    console.info(`server started on port 4040 (development)`); // eslint-disable-line no-console
+  const port = process.env.PORT
+  app.listen(port, () => {
+    console.info(`server started on port ${port} (development)`); // eslint-disable-line no-console
   });
+  
+  
 }
 
 module.exports = app;
--- src/test/component.js
+++ src/test/component.js
@@ -21,7 +21,7 @@ after((done) => {
   done();
 });
 
-describe('## Thing APIs', () => {
+describe('## Component APIs', () => {
   // TODO: Implement more tests.
   // test component
   const newComponent = {
@@ -35,53 +35,17 @@ describe('## Thing APIs', () => {
     chai.request(app)
       .get('/components')
       .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        console.log(res);
+        // if (err) {
+        //   return done(err);
+        // }
+        // console.log(res);
         if (res) {
           assert.equal(res.status, 200);
  
         }
         
-        return done();
+        // return done(err);
       })
   });
 
-  // it('Should create with valid attributes at POST /posts/new', function(done) {
-  //   // Checks how many posts there are now
-  //   Component.countDocuments()
-  //   .then(function (initialDocCount) {
-  //     chai
-  //     .request(app)
-  //     .post("/components/add/component")
-  //     // This line fakes a form post,
-  //     // since we're not actually filling out a form
-  //     .set("content-type", "application/x-www-form-urlencoded")
-  //     // Make a request to create another
-  //     .send(newComponent)
-  //     .then(function (res) {
-  //       Component.countDocuments()
-  //       .then(function (newDocCount) {
-  //         // Check that the database has one more post in it
-  //         expect(res).to.have.status(200);
-  //         // Check that the database has one more post in it
-  //         expect(newDocCount).to.be.equal(initialDocCount + 1)
-  //         done();
-  //       })
-  //       .catch(function (err) {
-  //         done(err);
-  //       });
-  //     })
-  //     .catch(function (err) {
-  //       done(err);
-  //     });
-  //   })
-  //   .catch(function (err) {
-  //     done(err);
-  //   });
-  // });
-  // after(function () {
-  //   Component.findOneAndDelete(newComponent);
-  // });
 });
