67cc66a2626fb4fea816403ba57ee03928941e2c
Anas <anbellouzi@gmail.com>
2020-05-11 16:07:03 -0700

    dockerizing 1

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -11,22 +11,10 @@ router.use('/components', componentRoutes);
 router.use('/auth', authRoutes);
 
 router.get('/', (req, res) => {
-    // res.json({"Welcome": "Active!", 
-    //         "GET List of Components": "/components", 
-    //         "GET specific component by name": "components/:name",
-    //         "POST a component": "components/add/component",
-    //         "UPDATE a component": "components/update/component",
-    //         "DELETE a component": "components/delete/:name",
-    //         "User sign up ": "auth/sign-up",
-    //         "User login ": "auth/login",
-    //         "User logout ": "auth/logout",
-    //         })
     res.render("index");
-
 })
 
 router.get('/healthcheck', (req,res) => {
-
     res.sendStatus(200);
 })
 
